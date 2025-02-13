c679b88dff22e835698ed0d1425d09d9f4a8ae2f
cosmin <doruivascu54@gmail.com>
2025-02-13 02:06:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "HansTz~JCE0hTqL#mlqKJnTQ3816lKuL1gS70r665AThXonVe9u5tlBM2pM",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -17,9 +17,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255756530143",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "0770811929",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "𝐕𝐎𝐑𝐓𝐄𝐗-𝐗𝐌𝐃",
+OWNER_NAME: process.env.OWNER_NAME || "cosmin",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© 𝐕𝐎𝐑𝐓𝐄𝐗-𝐗𝐌𝐃*",
 // add bot owner name    
